7013d972460b65d5c01195e026070fa64dfb9764
Sylvus10 <mbrooks1010@gmail.com>
2022-05-06 19:59:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ajtrbz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ajtrbz)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert(
+      'Congrats you fucking poor little fuck. You will be notified when this Phone goes to Peasant-level prices for sad little shits like you.'
+    );
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
